d34cc0660e581c6175fdc6502cd418433034971e
Camilo Mera <nemesis7845@hotmail.com>
2017-07-27 09:07:22 -0500

    Database

--- play-scala-seed/app/models/user.scala
+++ play-scala-seed/app/models/user.scala
@@ -2,6 +2,9 @@ package models
 
 import play.api.data.Form
 import play.api.data.Forms._
+import slick.basic.DatabaseConfig
+import slick.jdbc.JdbcProfile
+import slick.jdbc.MySQLProfile.api._
 
 case class User(id: Int, username: String, password: String)
 
@@ -16,3 +19,34 @@ object LoginForm{
 }
 
 case class UserRegisterForm(username: String, password: String, repeatedPassword: String)
+
+object RegisterForm{
+  val form = Form(
+    mapping(
+      "username" -> nonEmptyText,
+      "password" -> nonEmptyText,
+      "repeatedPassword" -> nonEmptyText
+    )(UserRegisterForm.apply)(UserRegisterForm.unapply)
+  )
+}
+
+trait Db {
+  val config: DatabaseConfig[JdbcProfile]
+  val db : JdbcProfile#Backend#Database = config.db
+}
+
+trait UsersTable{ this : Db =>
+  import config.profile.api._
+
+  class Users(tag: Tag) extends Table[User](tag, "users"){
+
+    def id = column[Int]("id", O.PrimaryKey, O.AutoInc)
+    def username = column[String]("username")
+    def password = column[String]("password")
+
+    override def * = (id, username, password) <>(User.tupled, User.unapply)
+  }
+
+  val users = TableQuery[Users]
+}
+
